9436eda6f5ec240d709e151582dfa1fb48080527
srima karekal <srima589.karekal@gmail.com>
2016-04-27 10:49:00 +0530

    commit

---
+++ src/Area.java
@@ -0,0 +1,11 @@
+
+public class Area {
+	int area;
+public void rectangle(int length,int breadth){
+	System.out.println("area of the rectangle:"+(length*breadth));
+}
+public void triangle(int length,int breadth)
+{
+	System.out.println("area of a triangle:"+(0.5*length*breadth));
+}
+}
---
+++ src/RectTri.java
@@ -0,0 +1,11 @@
+
+public class RectTri extends Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Area ap=new Area();
+ap.rectangle(5,6);
+ap.triangle(5, 4);
+	}
+
+}
